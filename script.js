a1fc6969bafbabf0dc822714abf3dc1c23d75194
Ali Safaa <alisafaa@Alis-MacBook-Pro.local>
2023-09-07 15:41:42 +0300

    new update

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-async function partition(arr, low, heigh) {
-  let pivot = arr[heigh];
-  let i = low - 1;
-  for (let j = low; j < heigh; j++) {
+let h1 = document.querySelector('h1');
+
+async function partition(arr, start, end) {
+  let pivot = arr[end];
+  let i = start - 1;
+  for (let j = start; j < end; j++) {
     if (arr[j] < pivot) {
       i++;
       [arr[i], arr[j]] = [arr[j], arr[i]];
     }
   }
-  [arr[i + 1], arr[heigh]] = [arr[heigh], arr[i + 1]];
+  [arr[i + 1], arr[end]] = [arr[end], arr[i + 1]];
   return i + 1;
 }
 
-async function quickSort(arr, low, heigh) {
-  if (low < heigh) {
-    await sleep(1000);
-    let pi = await partition(arr, low, heigh);
-    await quickSort(arr, low, pi - 1);
-    await quickSort(arr, pi + 1, heigh);
+async function quickSort(arr, start, end) {
+  if (start < end) {
+    await sleep(500);
+    h1.innerHTML = `[${arr}]`;
+    let pi = await partition(arr, start, end);
+    await quickSort(arr, start, pi - 1);
+    await quickSort(arr, pi + 1, end);
   }
 }
 
-let arr = [10, 7, 8, 3, 1, 5, 11, 22, 55, 9];
-let N = arr.length;
+let arr = [10, 7, 8, 5, 3];
+let num = arr.length;
 
-quickSort(arr, 0, N - 1);
+quickSort(arr, 0, num - 1);
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
